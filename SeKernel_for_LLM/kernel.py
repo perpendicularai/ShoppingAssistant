89bc3ef625f8104a066c7deae22ff231ea29ca13
perpendicularai <fahd@perpendicular.web.za>
2024-07-28 11:31:28 -0700

    Updated shopassist.py and kernel.py

--- SeKernel_for_LLM/kernel.py
+++ SeKernel_for_LLM/kernel.py
@@ -7,15 +7,23 @@ class Model(str):
      pass
 
 # Template function that takes a string and two objects as input and returns a list
-def template(prompt:str, plugin, context):
+def shopTemplate(prompt:str, plugin, context):
      my_list = MyList
      my_list = [
           {"role": "system", "content": plugin},
-          {"role": "user", "content": f'Using this data {context}, respond to this prompt {prompt}'},
+          {"role": "user", "content": f'Using this data {context}, respond to this prompt {prompt}.'},
+          ]
+     return my_list
+
+def chatTemplate(prompt:str, plugin):
+     my_list = MyList
+     my_list = [
+          {"role": "system", "content": plugin},
+          {"role": "user", "content": f'{prompt}.'},
           ]
      return my_list
 
 # Model function that returns the model as a string
 def model():
-     model = Model("<PATH TO GGUF MODEL>")
+     model = Model("C:\\Users\\RackUnit-1\\Models\\MetaAI\\meta-llama-3.1-8b-instruct-q4_k_m.gguf")
      return model
